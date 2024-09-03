0162d1585f71563ceb010ec3802768fd1f409100
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2024-09-03 13:57:58 -0400

    egui hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::*;
+use egui::CentralPanel;
+
+struct Boxit {}
+
+impl eframe::App for Boxit {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.label("Hello, World!");
+        });
+    }
+}
+
+fn main() -> eframe::Result<(), eframe::Error> {
+    run_native(
+        "Boxit", 
+        NativeOptions::default(), 
+        Box::new(|_cc| {
+            Ok(Box::new(Boxit {}))
+        })
+    )
 }
