bed03e0342c35ad4da3eaa6d1624b231ef704feb
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2024-09-04 00:36:42 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,26 @@ struct Boxit {}
 
 impl Boxit {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
-        // Restore app state using cc.storage (requires the "persistence" feature).
-        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-        // for e.g. egui::PaintCallback.
         Self::default()
     }
 }
 
 impl eframe::App for Boxit {
-   fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-       egui::CentralPanel::default().show(ctx, |ui| {
-           ui.heading("Hello World!");
-       });
-   }
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default()
+            .show(ctx, |ui| {
+                ui.heading("Hello World!");
+            });
+    }
 }
 
 fn main() -> eframe::Result<(), eframe::Error> {
-    let native_options = eframe::NativeOptions::default();
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_decorations(true) // change later
+            .with_inner_size([400.0, 300.0]),
+
+        ..Default::default()
+    };
     eframe::run_native("Boxit", native_options, Box::new(|cc| Ok(Box::new(Boxit::new(cc)))))
 }
