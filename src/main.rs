c8cea946fb0a8f4cf3f1250c5722b5f0f7d572a6
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2024-09-04 13:36:50 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use eframe::egui;
+use eframe::{egui::{menu, Align, Button, Context, Label, Layout, TopBottomPanel, ViewportCommand, CentralPanel, ViewportBuilder}, Frame};
 
 #[derive(Default)]
 struct Boxit {}
@@ -7,21 +7,38 @@ impl Boxit {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         Self::default()
     }
+    
+    fn render_title_bar(&self, ctx: &Context) {
+        TopBottomPanel::top("title_bar").show(ctx, |ui| {
+            ui.add_space(5.);
+            menu::bar(ui, |ui| {
+                ui.with_layout(Layout::left_to_right(Align::Center), |ui| {
+                ui.add(Label::new("❏"));
+                });
+                ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
+                    if ui.add(Button::new("✖")).clicked() {
+                        ui.ctx().send_viewport_cmd(ViewportCommand::Close);
+                    }
+                });
+            });
+            ui.add_space(5.);
+        });
+    }
 }
 
 impl eframe::App for Boxit {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default()
-            .show(ctx, |ui| {
-                ui.heading("Hello World!");
-            });
+    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
+        self.render_title_bar(ctx);
+        CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+        });
     }
 }
 
 fn main() -> eframe::Result<(), eframe::Error> {
     let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_decorations(true) // change later
+        viewport: ViewportBuilder::default()
+            .with_decorations(false)
             .with_inner_size([400.0, 300.0]),
 
         ..Default::default()
