5e0d5b6b4bc5b01f4b96a84159aab13e1314ceac
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2024-09-03 23:09:52 -0400

    remove egui separately and better hello world

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
-use eframe::*;
-use egui::CentralPanel;
+use eframe::egui;
 
+#[derive(Default)]
 struct Boxit {}
 
-impl eframe::App for Boxit {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
-        CentralPanel::default().show(ctx, |ui| {
-            ui.label("Hello, World!");
-        });
+impl Boxit {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::default()
     }
 }
 
+impl eframe::App for Boxit {
+   fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+       egui::CentralPanel::default().show(ctx, |ui| {
+           ui.heading("Hello World!");
+       });
+   }
+}
+
 fn main() -> eframe::Result<(), eframe::Error> {
-    run_native(
-        "Boxit", 
-        NativeOptions::default(), 
-        Box::new(|_cc| {
-            Ok(Box::new(Boxit {}))
-        })
-    )
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("Boxit", native_options, Box::new(|cc| Ok(Box::new(Boxit::new(cc)))))
 }
